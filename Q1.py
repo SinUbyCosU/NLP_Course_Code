b4baf85dd4a869a7b490319b79ef2653da43970f
SinUbyCosU <yadav23@iiserb.ac.in>
2026-02-17 22:36:01 +0530

    k

--- Q1.py
+++ Q1.py
@@ -2,10 +2,18 @@
 #This reads both files and prints the unique words (vocabulary) for each.
 
 import pandas as pd
-from sklearn.feature-extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 train_df=pd.read_csv("mental_health_train.csv")
 test_df=pd.read_csv("mental_health_test.csv")
 
 tfidf=TfidfVectorizer(stop_words="english")
 print("Train Vocabulary:")
+tfidf.fit(train_df['text'])
+print(tfidf.get_feature_names_out())
+print("Training Matrix Shape:", tfidf.transform(train_df['text']).shape)
+
+print("Training corpus")
+tfidf_test=TfidfVectorizer(stop_words='english')
+tfidf_test.fit(test_df['text'])
+print("Test matrix shape:", tfidf_test.transform(test_df['text']).shape)
