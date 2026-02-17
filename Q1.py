10c8a3c5198f121547395b62bcc7d7460e5814fb
SinUbyCosU <yadav23@iiserb.ac.in>
2026-02-17 22:21:03 +0530

    u

--- Q1.py
+++ Q1.py
@@ -1,2 +1,11 @@
 #TF-IDF Vocabulary
-#
+#This reads both files and prints the unique words (vocabulary) for each.
+
+import pandas as pd
+from sklearn.feature-extraction.text import TfidfVectorizer
+
+train_df=pd.read_csv("mental_health_train.csv")
+test_df=pd.read_csv("mental_health_test.csv")
+
+tfidf=TfidfVectorizer(stop_words="english")
+print("Train Vocabulary:")
