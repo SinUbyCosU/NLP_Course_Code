e96e87a84a8de671c366c86a452c6552865ca7f8
SinUbyCosU <yadav23@iiserb.ac.in>
2026-03-12 12:22:51 +0530

    abs

--- Neural_OOPS.py
+++ Neural_OOPS.py
@@ -9,4 +9,10 @@ class MyNeuralNetworks(nn.Module):
         self.layer1=nn.Linear(in_features=10,out_features=5)
         self.layer2=nn.Linear(in_features=5, out_features=1)
 
+#Abstraction
+    def forward(self,x):
+        x=torch.relu(self.layer1(x))
+        x=self.layer2(x)
+        return x
+
 model=MyNeuralNetworks()
