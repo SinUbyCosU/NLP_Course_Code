7cc10cd97fd1876cd86738b6631e027827819b16
SinUbyCosU <yadav23@iiserb.ac.in>
2026-03-26 17:27:30 +0530

    iterative dfs

--- discard_practise_file.py
+++ discard_practise_file.py
@@ -8,3 +8,19 @@ def dfs(graph, node, visited=None):
         if neighbour not in visited:
             dfs(graph, neighbour, visited)
     return visited
+
+def dfs_iterative(graph, start):
+    visited=set()
+    stack=[start]
+
+    while stack:
+        node=stack.pop()
+        
+        if node not in visited:
+            visited.add(node)
+            print(node)
+
+            for neighbour in graph[node]:
+                if neighbour not in visited:
+                    stack.append(neighbour)
+    return visited
