5b4a7e523ed3ee7fae0ba0b6885825faf7826850
SinUbyCosU <yadav23@iiserb.ac.in>
2026-03-26 16:37:33 +0530

    rescursive

--- DFS.py
+++ DFS.py
@@ -1,1 +1,12 @@
 #AI quiz practice
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited =set()
+    visited.add(node)
+    print(node)
+
+    for neighbour in graph[node]:
+        if neighbour not in visited:
+            dfs(graph, neighbour, visited)
+
+    return visited
