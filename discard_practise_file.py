c23489ea7f3085bc10113b6d77852224234dbcf5
SinUbyCosU <yadav23@iiserb.ac.in>
2026-03-26 16:44:29 +0530

    discard code

---
+++ discard_practise_file.py
@@ -0,0 +1,10 @@
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited=set()
+    visited.add(node)
+    print(node)
+
+    for neighbour in graph[node]:
+        if neighbour not in visited:
+            dfs(graph, neighbour, visited)
+    return visited
